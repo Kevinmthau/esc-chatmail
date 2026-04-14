47fe4a05d04ad3038a26abab0dbc1f7a83a46740
kevinmthau <kmthau@gmail.com>
2026-04-14 12:21:41 -0400

    Retry Google Drive metadata fetches after blocked or failed loads

--- esc-chatmailTests/Preview/GoogleDriveSharedFileMetadataProviderTests.swift
+++ esc-chatmailTests/Preview/GoogleDriveSharedFileMetadataProviderTests.swift
@@ -2,6 +2,11 @@ import XCTest
 @testable import esc_chatmail
 
 final class GoogleDriveSharedFileMetadataProviderTests: XCTestCase {
+    override func tearDown() {
+        TestURLProtocol.reset()
+        super.tearDown()
+    }
+
     func testParseMetadata_prefersOpenGraphTitleAndImage() {
         let link = SharedDocumentLink(
             id: "SharedDocumentLink.Kind.googleDoc|doc123",
@@ -50,7 +55,7 @@ final class GoogleDriveSharedFileMetadataProviderTests: XCTestCase {
         )
     }
 
-    func testParseMetadata_ignoresGenericSignInTitle() {
+    func testParseMetadata_returnsNilForSignInTitle() {
         let link = SharedDocumentLink(
             id: "SharedDocumentLink.Kind.googleSheet|sheet123",
             url: URL(string: "https://docs.google.com/spreadsheets/d/sheet123/edit")!,
@@ -66,11 +71,7 @@ final class GoogleDriveSharedFileMetadataProviderTests: XCTestCase {
 
         let metadata = GoogleDriveSharedFileMetadataProvider.parseMetadata(from: html, link: link)
 
-        XCTAssertEqual(metadata?.title, "Google Sheet")
-        XCTAssertEqual(
-            metadata?.thumbnailURL,
-            "https://drive.google.com/thumbnail?id=sheet123&sz=w1600"
-        )
+        XCTAssertNil(metadata)
     }
 
     func testParseMetadata_preservesApostrophesInDoubleQuotedMetaContent() {
@@ -110,4 +111,175 @@ final class GoogleDriveSharedFileMetadataProviderTests: XCTestCase {
 
         XCTAssertEqual(metadata?.title, "The \"Launch\" Plan")
     }
+
+    func testMetadata_retriesAfterBlockedPageResponse() async {
+        let link = SharedDocumentLink(
+            id: "SharedDocumentLink.Kind.googleDoc|doc123",
+            url: URL(string: "https://docs.google.com/document/d/doc123/edit?usp=sharing")!,
+            kind: .googleDoc
+        )
+        let provider = GoogleDriveSharedFileMetadataProvider(session: makeTestSession())
+
+        TestURLProtocol.responders = [
+            { request in
+                try Self.makeHTMLResponse(
+                    for: request,
+                    html: """
+                    <html>
+                    <head>
+                    <title>Sign in - Google Accounts</title>
+                    </head>
+                    </html>
+                    """
+                )
+            },
+            { request in
+                try Self.makeHTMLResponse(
+                    for: request,
+                    html: """
+                    <html>
+                    <head>
+                    <meta property="og:title" content="Launch Plan - Google Docs">
+                    </head>
+                    </html>
+                    """
+                )
+            }
+        ]
+
+        let firstResult = await provider.metadata(for: link)
+        let secondResult = await provider.metadata(for: link)
+
+        XCTAssertEqual(firstResult, GoogleDriveSharedFileMetadataProvider.fallbackMetadata(for: link))
+        XCTAssertEqual(secondResult.title, "Launch Plan")
+        XCTAssertEqual(TestURLProtocol.requestedURLs, [link.url, link.url])
+    }
+
+    func testMetadata_fetchesNewURLVariantAfterInitialFailureForSameResource() async {
+        let failedLink = SharedDocumentLink(
+            id: "SharedDocumentLink.Kind.googleDoc|doc123",
+            url: URL(string: "https://docs.google.com/document/d/doc123/edit?usp=sharing")!,
+            kind: .googleDoc
+        )
+        let recoveredLink = SharedDocumentLink(
+            id: failedLink.id,
+            url: URL(string: "https://docs.google.com/document/d/doc123/edit?usp=sharing&resourcekey=abc")!,
+            kind: .googleDoc
+        )
+        let provider = GoogleDriveSharedFileMetadataProvider(session: makeTestSession())
+
+        TestURLProtocol.responders = [
+            { request in
+                try Self.makeResponse(for: request, statusCode: 404, body: "")
+            },
+            { request in
+                try Self.makeHTMLResponse(
+                    for: request,
+                    html: """
+                    <html>
+                    <head>
+                    <meta property="og:title" content="Recovered Title - Google Docs">
+                    </head>
+                    </html>
+                    """
+                )
+            }
+        ]
+
+        let firstResult = await provider.metadata(for: failedLink)
+        let secondResult = await provider.metadata(for: recoveredLink)
+
+        XCTAssertEqual(
+            firstResult,
+            GoogleDriveSharedFileMetadataProvider.fallbackMetadata(for: failedLink)
+        )
+        XCTAssertEqual(secondResult.title, "Recovered Title")
+        XCTAssertEqual(TestURLProtocol.requestedURLs, [failedLink.url, recoveredLink.url])
+    }
+
+    private func makeTestSession() -> URLSession {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [TestURLProtocol.self]
+        return URLSession(configuration: configuration)
+    }
+
+    private static func makeHTMLResponse(
+        for request: URLRequest,
+        html: String
+    ) throws -> (HTTPURLResponse, Data) {
+        try makeResponse(
+            for: request,
+            statusCode: 200,
+            body: html
+        )
+    }
+
+    private static func makeResponse(
+        for request: URLRequest,
+        statusCode: Int,
+        body: String
+    ) throws -> (HTTPURLResponse, Data) {
+        guard let url = request.url,
+              let response = HTTPURLResponse(
+                url: url,
+                statusCode: statusCode,
+                httpVersion: nil,
+                headerFields: ["Content-Type": "text/html; charset=utf-8"]
+              ) else {
+            throw NSError(domain: NSURLErrorDomain, code: NSURLErrorBadURL)
+        }
+
+        return (response, Data(body.utf8))
+    }
+}
+
+private final class TestURLProtocol: URLProtocol {
+    typealias Responder = (URLRequest) throws -> (HTTPURLResponse, Data)
+
+    private static let lock = NSLock()
+    static var responders: [Responder] = []
+    static var requestedURLs: [URL] = []
+
+    static func reset() {
+        lock.lock()
+        responders = []
+        requestedURLs = []
+        lock.unlock()
+    }
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        request
+    }
+
+    override func startLoading() {
+        let responder: Responder?
+        let requestURL = request.url
+
+        Self.lock.lock()
+        if let requestURL {
+            Self.requestedURLs.append(requestURL)
+        }
+        responder = Self.responders.isEmpty ? nil : Self.responders.removeFirst()
+        Self.lock.unlock()
+
+        guard let responder else {
+            client?.urlProtocol(self, didFailWithError: NSError(domain: NSURLErrorDomain, code: NSURLErrorBadServerResponse))
+            return
+        }
+
+        do {
+            let (response, data) = try responder(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+
+    override func stopLoading() {}
 }
